020ac863c63647d7b367ce8c250c102826f2aac7
Ian Shepherd <ianshepherd77@gmail.com>
2019-09-13 17:58:31 +0100

    added species component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <h1>Ghibli Films</h1>
     <div>
       <film-detail v-bind:film="selectedFilm"></film-detail>
+      <species-select v-bind:speciesList="speciesList"></species-select>
       <films-list v-bind:films="films"></films-list>
     </div>
   </div>
@@ -11,6 +12,7 @@
 <script>
 import FilmsList from './components/FilmsList.vue';
 import FilmDetail from './components/FilmDetail.vue';
+import SpeciesSelect from './components/SpeciesSelect.vue';
 import { eventBus } from "./main.js";
 
 export default {
@@ -18,13 +20,18 @@ export default {
   data(){
     return {
       films: [],
-      selectedFilm: null
+      selectedFilm: null,
+      speciesList: []
     };
   },
   mounted(){
     fetch('https://ghibliapi.herokuapp.com/films')
     .then(result => result.json())
     .then(films => this.films = films)
+
+    fetch('https://ghibliapi.herokuapp.com/species?fields=id,name')
+    .then(result => result.json())
+    .then(speciesList => this.speciesList = speciesList)
 
     eventBus.$on('film-selected', (film) => {
       this.selectedFilm = film
@@ -33,6 +40,7 @@ export default {
   components: {
     "films-list": FilmsList,
     "film-detail": FilmDetail,
+    "species-select": SpeciesSelect
   }
 }
 </script>
---
+++ src/components/SpeciesSelect.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <select v-model="selectedSpecies">
+    <option v-for="species in speciesList" v-bind:value="species">
+      {{ species.name}}
+    </option>
+
+
+  </select>
+</template>
+
+<script>
+
+export default {
+  name: 'species-select',
+  props: ['speciesList']
+  }
+
+</script>
+<style lang="css" scoped>
+</style>
